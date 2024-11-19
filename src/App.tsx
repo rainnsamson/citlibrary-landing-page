5d193275872c2aa52fd09e0300ae885a1aad36fe
rainnsamson <raineriussamson@gmail.com>
2024-11-19 11:04:40 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import FeatureCard from './components/FeatureCard';
 import { FaEnvelope, FaFacebook, FaGlobe, FaPhone } from 'react-icons/fa';
